1f9bf2aa4ac7d52fafd1bdd76c786d78fbd82037
Caous <caous.g@gmail.com>
2025-09-30 21:44:02 -0300

    refactor: update user information in UserInfo component

--- src/app/trainingStudent/page.tsx
+++ src/app/trainingStudent/page.tsx
@@ -4,93 +4,12 @@ import { OverviewCardsSkeleton } from "../(home)/_components/overview-cards/skel
 import { Sidebar } from "@/components/Layouts/sidebar";
 import { Header } from "@/components/Layouts/header";
 import MenuMobile from "@/components/MenuMobile";
-<<<<<<< HEAD
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Treinos",
-};
-
-export default async function TraningStudent() {
-  const exercisesGroups = [
-    {
-      groupName: "Treino A",
-      groupImage: "/exercicio.jpg",
-      exercises: [
-        {
-          id: 1,
-          name: "Perna",
-          reps: 12,
-          weight: "60kg",
-          time: "01:30",
-          media: "/exercicio.jpg",
-          checked: true,
-        },
-      ],
-    },
-    {
-      groupName: "Treino B",
-      groupImage: "/exercicio2.jpg",
-      exercises: [
-        {
-          id: 3,
-          name: "Perna",
-          reps: 10,
-          weight: "80kg",
-          time: "02:00",
-          media: "/exercicio.jpg",
-          checked: false,
-        },
-      ],
-    },
-    {
-      groupName: "Treino C",
-      groupImage: "/exercicio2.jpg",
-      exercises: [
-        {
-          id: 4,
-          name: "Perna",
-          reps: 10,
-          weight: "80kg",
-          time: "02:00",
-          media: "/exercicio.jpg",
-          checked: false,
-        },
-        {
-          id: 5,
-          name: "Perna",
-          reps: 8,
-          weight: "40kg",
-          time: "01:00",
-          media: "/exercicio2.jpg",
-          checked: false,
-        },
-      ],
-    },
-    {
-      groupName: "Treino D",
-      groupImage: "/exercicio.jpg",
-      exercises: [
-        {
-          id: 6,
-          name: "Perna",
-          reps: 10,
-          weight: "80kg",
-          time: "02:00",
-          media: "/exercicio2.jpg",
-          checked: false,
-        },
-      ],
-    },
-  ];
-=======
 import TrainingStudentClient from "./client";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Treinos",
 };
->>>>>>> feat/back-end-app-initial
 
 export default function TrainingStudentPage() {
   return (
